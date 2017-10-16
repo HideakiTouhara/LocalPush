55c89c49584e23a61d9c332ad3cbabfb62c5484e
HideakiTouhara <hideee6723@gmail.com>
2017-10-16 19:32:34 +0900

    Push Local Notification

--- LocalPush_Learn/ViewController.swift
+++ LocalPush_Learn/ViewController.swift
@@ -9,18 +9,54 @@
 import UIKit
 import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var whoTextField: UITextField!
+    @IBOutlet weak var honbunTextField: UITextField!
+    
+    var resultText = ""
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        whoTextField.delegate = self
+        honbunTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - TextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        resultText = whoTextField.text! + ":" + honbunTextField.text!
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: - アプリケーションロジック
+    func startPush() {
+        let content = UNMutableNotificationContent()
+        content.body = resultText
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 4.0, repeats: false)
+        
+        let requestIdentifier = "LinePush"
+        let request = UNNotificationRequest(identifier: requestIdentifier, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+            
+        })
+        
+    }
 
+    // MARK: - IBAction
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        startPush()
+    }
 
 }
 
